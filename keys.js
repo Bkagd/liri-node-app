406cdaf66b45326193ba142c4a709e17d2851860
Alan Drake <alangeorgedrake@gmail.com>
2018-01-18 15:04:34 -0700

    added files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("this is loaded");
+
+var twitterKeys = {
+    consumer_key: "vCw3B79cOFVex1OQJTZi3AqRk",
+    consumer_secret: "7Gytqzux2PWwiux8eqJEYE0snQnxrlCfpkMqkxABPT5mjjF4tf",
+    access_token_key: "954092391148937216-KKXhwncq1ah2TK9fhVo3MO3eyPhos1j",
+    access_token_secret: "whyxpqjBjwn5qR7W31oeCv0nFHY4uwQwy4rHT0XGIz1pe",
+}
+
+module.exports = twitterKeys;
